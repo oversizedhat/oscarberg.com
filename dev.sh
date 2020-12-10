9c8825869bf3c60727304d7cfa55a9f610371403
oversizedhat <oscar@oscarberg.com>
2020-12-10 21:56:16 +0100

    added serve of drafts, and npm commands for version tags

--- dev.sh
+++ dev.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 export DEV_SERVER_PRIVATE_IP=$(hostname -I | awk '{print $1}')
 
+# build drafts
+if [ "$1" == "drafts" ]
+then
+  export HUGO_FLAGS="-D"
+fi
+
 # launch browser
 sensible-browser $DEV_SERVER_PRIVATE_IP:8080
 
